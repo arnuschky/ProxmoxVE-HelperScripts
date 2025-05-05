1bf7fcd209a6587aab9127264b80aac4b7d0c7d9
Arne Brutschy <arne@cryptosphere-systems.com>
2025-05-05 12:43:28 +0200

    Add debug info

--- ct/mktxp.sh
+++ ct/mktxp.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: arnuschky
